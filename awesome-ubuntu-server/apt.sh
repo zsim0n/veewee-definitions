a3f325b41cbc9fa6b29283fae0947ac9cdce4a33
Zoltan Simon <zoltan@zoltansimon.me>
2014-05-24 00:54:11 +0200

    add more apt, language, mc, curl, git, wget

--- awesome-ubuntu-server/apt.sh
+++ awesome-ubuntu-server/apt.sh
@@ -2,6 +2,12 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
 apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
+apt-get -y install language-pack-en
 apt-get -y install vim
 apt-get -y install dkms
 apt-get -y install nfs-common
+apt-get -y install git
+apt-get -y install wget
+apt-get -y install mc
+apt-get -y install curl
+
